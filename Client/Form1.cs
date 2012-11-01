f85d0fe1efbc3aba46f34103f258bce167976a40
James Gubler <gubimail@gmail.com>
2012-11-01 14:33:28 -0600

    working on tree view cDirMap
    not quite ready, but i think it might be close, could probably use some
    tweaks, i'm sorry but i've come down sick and won't be able to make it
    to the group meeting at 8, hopefully i'm at the lab tomorrow and we can
    talk

--- Client/Form1.cs
+++ Client/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
 
 namespace SuperSimpleSync
 {
@@ -14,11 +15,38 @@ namespace SuperSimpleSync
         public Form1()
         {
             InitializeComponent();
+            
+        }
+        // display the tree, get the names of the directories and files and put them in the tree
+        // doesn't work yet, probably needs a little tweaking
+        private void InitializeTreeView(DirectoryInfo di)
+        {
+            cDirMap.BeginUpdate();
+            cDirMap.Nodes.Clear();
+            DirectoryInfo[] dirs = di.GetDirectories();
+            foreach (DirectoryInfo dir in dirs)
+            {
+                TreeNode dNode = new TreeNode(dir.Name);
+                cDirMap.Nodes.Add(dNode);
+                FileInfo[] files = dir.GetFiles();
+                DirectoryInfo[] subdirs1 = dir.GetDirectories();
+                foreach (DirectoryInfo subdir in subdirs1)
+                {
+                    TreeNode fNode = new TreeNode(subdir.Name);
+                    cDirMap.Nodes.Add(fNode);
+                }
+                foreach (FileInfo file in files)
+                {
+                    TreeNode fNode = new TreeNode(file.Name);
+                    cDirMap.Nodes.Add(fNode);
+                }
+            }
+            cDirMap.EndUpdate();
         }
 
         private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
         {
-
+            //accidently made this by double clicking on treeview, haven't deleted yet
         }
 
         private void cSync_Click(object sender, EventArgs e)
@@ -28,6 +56,8 @@ namespace SuperSimpleSync
                 Program p = new Program();
                 //p.setAccId(cAcc.Text);
                 p.setLocalStorage(cDir.Text);
+                DirectoryInfo DI = p.getLocalStorage();
+                InitializeTreeView(DI);
                 //tries to sync with server
                 try
                 {
